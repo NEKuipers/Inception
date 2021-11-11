d75b62aec5a4decb197e0d41d2820b0baad29beb
Nick Kuipers <nkuipers@f0r4s4.codam.nl>
2021-11-11 15:45:59 +0100

    update

--- srcs/wordpress/src/wp-config.php
+++ srcs/wordpress/src/wp-config.php
@@ -10,14 +10,14 @@ define( 'DB_CHARSET', 'utf8mb4' );
 define( 'DB_COLLATE', '' );
 
 
-define('AUTH_KEY',         'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('SECURE_AUTH_KEY',  'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('LOGGED_IN_KEY',    'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('NONCE_KEY',        'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('AUTH_SALT',        'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('SECURE_AUTH_SALT', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('LOGGED_IN_SALT',   'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
-define('NONCE_SALT',       'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbb');
+define('AUTH_KEY',         ':ZH@+<zTi!!1eOC}+b@53tKa;Qz<[:XOpxQ/@-cAYI%(SaD1~PP5t{T]}!S+TYP ');
+define('SECURE_AUTH_KEY',  'CI#>.Q};8$B.||4qpKY`(v5T_+O|Fo6mS7%zUhRwAO>>(>V yT-&vt%S@*s>|aCX');
+define('LOGGED_IN_KEY',    '4Bni|ERl30C#B5 5`Khyw%T%62e?ziB8+0T2 x1m#duhl.!Nz3FJaRs?l$s19<$B');
+define('NONCE_KEY',        'F`}Kr86]y.w^MKi0<#E.U 5uyP5v1`%UZL#.=|PHh2`W;0b%iX/28Os(}tI++`bX');
+define('AUTH_SALT',        'shM_[ &,UmDq_2*!@O=9XiZbF9jk$3-u#>WPjH6My,BC:-PnicM5)Mhg<ZrBxM#b');
+define('SECURE_AUTH_SALT', '-}bkE$c|XpN6E?S*F}Tpb+-(-^A:|X`G<T|asN6Ve[DC;GpXr7FI$V}!@9~txx{U');
+define('LOGGED_IN_SALT',   'qE.I486Qcwvy(|bSeIq&f[G:pvuw2, s:Wg)|*IDj+[`i;+P-OC#&,8S-O.nD`)L');
+define('NONCE_SALT',       'vwYTd|HJWE4_NkDPaOAYbewDGQqI;VuL~I-Fqu{-Pp! {U~s#k632ST!oN?w%O7#');
 
 $table_prefix = 'wp_';
 
